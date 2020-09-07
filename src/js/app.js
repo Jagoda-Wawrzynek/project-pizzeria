33296ac111968d0ab1f43b64ae4f1bad2ecbd663
Jagoda Wawrzynek <jagoda.wawrzynek@gmail.com>
2020-09-07 20:11:19 +0200

    Add subpage

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,68 @@
 import {Product} from './components/Product.js';
 import {Cart} from './components/Cart.js';
-import {select, settings} from './settings.js';
+import {select, settings, classNames} from './settings.js';
 
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 //const { init } = require("browser-sync");
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+
+    //thisApp.activatePage(thisApp.pages[0].id);
+    let pagesMatchingHash = [];
+
+    if(window.location.hash.length > 2) {
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      for(let page of thisApp.pages){
+        if(page.id == idFromHash){
+          pagesMatchingHash.push(page);
+        }
+      }
+    }
+
+    if(pagesMatchingHash.length){
+      thisApp.activatePage(pagesMatchingHash[0].id);
+    } else {
+      thisApp.activatePage(thisApp.pages[0].id);
+    }
+
+    thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /*get page if from href attribuet*/
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /*run thisAll.activatePage with that id*/
+        thisApp.activatePage(id);
+        //change URL hash 
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /*add class "active" to matching pages, remove from non-matching*/
+    for(let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    console.log('newPage', thisApp.pages);
+    /*add class "active" to matching links, remove from non-matching*/
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+    console.log('newLink', thisApp.navLinks);
+  },
+
   initMenu: function(){
     const thisApp = this;
     //console.log('thisApp.data:', thisApp.data);
@@ -57,7 +113,8 @@ const app = {
     //console.log('classNames:', classNames);
     //console.log('settings:', settings);
     //console.log('templates:', templates);
-      
+    
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
